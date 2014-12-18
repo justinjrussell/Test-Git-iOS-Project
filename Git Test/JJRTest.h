52cea4e8bc20f2a14d9ae65bf0b97c78e5d74097
Justin Russell <justinrussell@Justins-iMac.local>
2014-12-17 22:03:04 -0800

    Added Test Class

---
+++ Git Test/JJRTest.h
@@ -0,0 +1,13 @@
+//
+//  JJRTest.h
+//  Git Test
+//
+//  Created by Justin Russell on 12/17/14.
+//  Copyright (c) 2014 Justin Russell. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JJRTest : NSObject
+
+@end
---
+++ Git Test/JJRTest.m
@@ -0,0 +1,13 @@
+//
+//  JJRTest.m
+//  Git Test
+//
+//  Created by Justin Russell on 12/17/14.
+//  Copyright (c) 2014 Justin Russell. All rights reserved.
+//
+
+#import "JJRTest.h"
+
+@implementation JJRTest
+
+@end
